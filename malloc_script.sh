57fd2ebc3b5fb394dd9c24c68d40e1a3d35ec8bb
Amelie LALLEMAND <alallema@e1r4p12.42.fr>
2018-11-28 20:49:20 +0100

    clean

--- malloc_script.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-export LD_LIBRARY_PATH=$(pwd)
-#echo $LD_LIBRARY_PATH
-export DYLD_LIBRARY_PATH=$(pwd)
-export DYLD_FALLBACK_LIBRARY_PATH=$(pwd)
-export DYLD_INSERT_LIBRARIES="libft_malloc.so"
-export DYLD_FORCE_FLAT_NAMESPACE=1
-$@
